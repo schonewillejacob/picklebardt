a648330994f8a78e2f51ca5b90984306390a71c1
Jacob <winkschonewille@gmail.com>
2024-07-14 13:49:38 -0700

    exit signal connected

--- nodes/ui/menu/menu.gd
+++ nodes/ui/menu/menu.gd
@@ -1,12 +1,16 @@
 extends VBoxContainer
 # Splash screen, prototyping menus:
 
-@onready var exitBufferTimer = $expanding_options_header/End/Timer
+@onready var exitBufferTimer = $expanding_options_header/End/endTimer
+@onready var endSound = $expanding_options_header/End/endSound
+
+func _ready():
+	exitBufferTimer.timeout.connect(_on_exitBufferTimer)
 
 func _on_end_pressed():
 	#play sound
-	exitBufferTimer.start(1.5)
+	endSound.play()
+	exitBufferTimer.start()
 	
 func _on_exitBufferTimer():
 	get_tree().quit()
-	pass
