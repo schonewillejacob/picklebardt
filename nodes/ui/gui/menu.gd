7d517cec212cef143c559b42d00bac1f066cfc46
Jacob <winkschonewille@gmail.com>
2024-07-14 11:26:12 -0700

    menu refactor away from testing root control node.

--- nodes/ui/gui/menu.gd
+++ nodes/ui/gui/menu.gd
@@ -1,12 +1,5 @@
 extends VBoxContainer
-
+# MenuHome
 # Can be used at the start, or any time.
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	# Called when the node enters the scene tree for the first time.
