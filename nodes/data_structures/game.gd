bd72877f0fdb310990359fc22c346760190a7ad8
Jacob <winkschonewille@gmail.com>
2024-07-14 13:19:01 -0700

    exit button connection

--- nodes/data_structures/game.gd
+++ nodes/data_structures/game.gd
@@ -1,11 +1,10 @@
 extends Node
 
+# Saves state of game
+# Retains list of players
+# 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var matches : Match
+var players : Player
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
--- nodes/ui/gui/menu.gd
+++
@@ -1,5 +0,0 @@
-extends VBoxContainer
-# MenuHome
-# Can be used at the start, or any time.
-
-	# Called when the node enters the scene tree for the first time.
