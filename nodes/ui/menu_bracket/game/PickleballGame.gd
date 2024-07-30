9d480fd565e666591e81231af6135787e4824ab7
Jacob <winkschonewille@gmail.com>
2024-07-29 21:20:31 -0700

    menu_bracket properly packing PickleballGame scene. icon svg colour swap. Project config version injected into menu_home label

--- nodes/ui/menu_bracket/game/PickleballGame.gd
+++ nodes/ui/menu_bracket/game/PickleballGame.gd
@@ -4,9 +4,12 @@ class_name PickleballGame
 # These nodes are placed into the GameColumn node in menu_bracket
 
 const PATH_MATCH : String = "res://nodes/ui/menu_bracket/game/Match.tscn"
+var packedMatch : PackedScene
+
 @onready var matchContainer : GridContainer = $MatchContainer
 #@export var matchContainer : GridContainer
 var matchCount : int
+
 
 
 # Virtuals ####################################################
@@ -29,7 +32,7 @@ func create_matches():
 	if matchContainer == null:
 		push_error("matchContainer == null")
 	
-	var packedMatch = ResourceLoader.load_threaded_get(PATH_MATCH)
+	packedMatch = ResourceLoader.load_threaded_get(PATH_MATCH)
 	# TODO matchContainer doesn't exist when instantiated, figure out how to reference downwards w/in a child
 	for i in matchCount:
 		var instMatch = packedMatch.instantiate()
