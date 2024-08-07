13c718a222f775905a9cbd704595a7ea704e6414
Jacob <winkschonewille@gmail.com>
2024-08-07 12:28:41 -0700

    good restore, back to stable. spacers added to games. Name labels are got by Match(es) at runtime.

---
+++ nodes/ui/menu_bracket/match/Match.gd
@@ -0,0 +1,20 @@
+extends MarginContainer
+class_name Match
+
+# Holds Players
+
+@onready var nodePlayerLabel_0 : Label = $GridContainer/player_0
+@onready var nodePlayerLabel_1 : Label = $GridContainer/player_1
+@onready var nodePlayerLabel_2 : Label = $GridContainer/player_2
+@onready var nodePlayerLabel_3 : Label = $GridContainer/player_3
+
+# Virtuals ####################################################
+#func _init() -> void:
+#func _ready() -> void:
+#func _process(delta: float) -> void:
+
+
+# Signals #####################################################
+
+
+# Helpers #####################################################
