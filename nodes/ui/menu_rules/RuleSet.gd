b7cd58112cf14398f8ecb8e45196b4c4b3489cbc
Jacob <winkschonewille@gmail.com>
2024-08-11 21:40:26 -0700

    fixed ruleset exporting, guarded against edge cases

--- nodes/ui/menu_rules/RuleSet.gd
+++ nodes/ui/menu_rules/RuleSet.gd
@@ -1,16 +1,20 @@
 extends Node
 class_name RuleSet
 
-@export_group("Rules")
-@export var courtSize : int
-@export var courtsAvailable : int
-@export var shuffleSeed : int
+
+
+var courtSize : int
+var courtsAvailable : int
+var shuffleSeed : int
 var playerSlots : int = 0
 
 
 
 # Virtuals ####################################################
-func _init(courts_available : int, court_size : int, shuffle_seed = null):
+func _init(courts_available : int, court_size : int, shuffle_seed : int):
 	courtsAvailable = courts_available
 	playerSlots = courts_available * court_size
-	if shuffleSeed != null: shuffleSeed = shuffle_seed
+	shuffleSeed = shuffle_seed
+
+func _to_string() -> String:
+	return "courtSize: "+str(courtSize)+", courtsAvailable: "+str(courtsAvailable)+", playerSlots: "+str(playerSlots)+", hash(shuffleSeed): "+str(hash(shuffleSeed))
