8b9ce50f2814fac8112eacbb97493c608d5b1a94
Jacob <winkschonewille@gmail.com>
2025-01-02 15:20:19 -0800

    json import/export. add and remove names to export as participantList

--- nodes/ui/menu_participants/menu_participants.gd
+++ nodes/ui/menu_participants/menu_participants.gd
@@ -1,30 +1,82 @@
 extends LerpContainer
 
 ## Uses a gui to solicit player names.
-# 	uses this to provide an Array of PickleballPlayer objects
-#   PickleballPlayer objects must be assigned an int that has a unique binary representation w/ constructor.
+# 	Uses this to provide an Array of PickleballPlayer objects
+#   PickleballPlayer objects must be assigned an int that has a unique binary 
+#	representation w/ constructor.
 
 # served list of Players.
-var participantList : Array = []
+var participantList : Array[PickleballPlayer] = []
+var DEBUG_DUMMY : bool = false
+var slot_pool : Array[int] = [0]
+var slot_ongoing : int = 0
+@onready var node_add_player_line_edit : LineEdit = $MarginContainer/VBoxContainer/ColorRect/MarginContainer/menu_participants/HBoxContainer/AddPlayerLineEdit
+@onready var packed_player_template : PackedScene = load("res://nodes/ui/menu_participants/player_template.tscn")
+@onready var node_player_list : VBoxContainer = $MarginContainer/VBoxContainer/ColorRect/MarginContainer/menu_participants/TabContainer/Players/VBoxContainer
 
 signal AddPlayer
 
 
-# Virtuals ####################################################
+# VIRTUALS #####################################################################
 func _ready() -> void:
 	lerpDirection = -1
-	var dummyList_ : Array = make_dummy_list(20)
-	set_participantList(dummyList_)
 
 
 
-# Signals #####################################################
-func _on_addPlayer_pressed():
-	pass
+# SIGNALS #####################################################
+func _on_add_player_button_pressed() -> void:
+	var _new_player_name
+	# Manipulate LineEdit.
+	if node_add_player_line_edit.text == "": 
+		_new_player_name = "Player " + str(slot_ongoing)
+		slot_ongoing += 1
+	else:
+		_new_player_name = node_add_player_line_edit.text
+	node_add_player_line_edit.text = ""
+	
+	# Create player from template.
+	var _new_player = packed_player_template.instantiate()
+	if _new_player.get_node_or_null(
+		"PlayerDecor/PlayerTemplateMargin/PlayerContainer/PlayerNameLabel"
+		):
+		_new_player.get_node_or_null(
+		"PlayerDecor/PlayerTemplateMargin/PlayerContainer/PlayerNameLabel"
+		).text = _new_player_name 
+	_new_player.tree_exited.connect(_on_remove_player_button_pressed)
+	node_player_list.add_child(_new_player)
+	AddPlayer.emit()
+	
+	
 
+func _on_remove_player_button_pressed() -> void:
+	var _total = node_player_list.get_child_count()
+	if _total < 1: 
+		slot_ongoing = 0
+		return
 
+# METHODS #####################################################
+func create_from_control_nodes():
+	# Get nodes, use names and data for PickleBallPlayer objects
+	var _list : Array[PickleballPlayer] = []
+	var _id_binary_interation : int = 0b1
+	var _name : String = "player"
+	for _player in node_player_list.get_children():
+		# Guard clause: type
+		if _player is not PlayerTemplate: continue
+		if _player.get_node_or_null(
+			"PlayerDecor/PlayerTemplateMargin/PlayerContainer/PlayerNameLabel"
+			):
+			_name = _player.get_node_or_null(
+			"PlayerDecor/PlayerTemplateMargin/PlayerContainer/PlayerNameLabel"
+			).text
+		var _inst_player : PickleballPlayer = PickleballPlayer.new(
+			_name,
+			_id_binary_interation)
+		_id_binary_interation *= 2
+		_list.append(_inst_player)
+	participantList = _list
+	
 
-# Helpers #####################################################
 func make_dummy_list(length) -> Array:
 	var list_ : Array = []
 	var id_interations_ : int = 0b1
@@ -39,3 +91,45 @@ func make_dummy_list(length) -> Array:
 
 func set_participantList(new_list) -> void:
 	participantList = new_list
+
+func quickload_list():
+	var _file = FileAccess.open("user://player_list.dat", FileAccess.READ)
+	if _file == null:
+		return
+	
+	var _json_data = JSON.new()
+	var error = _json_data.parse(_file.get_as_text())
+	if error == OK:
+		for _player in _json_data.data:
+			var _inst_player_quickload = packed_player_template.instantiate()
+			if _inst_player_quickload.get_node_or_null(
+				"PlayerDecor/PlayerTemplateMargin/PlayerContainer/PlayerNameLabel"
+				):
+				_inst_player_quickload.get_node_or_null(
+				"PlayerDecor/PlayerTemplateMargin/PlayerContainer/PlayerNameLabel"
+				).text = str(_player)
+				
+				node_player_list.add_child(_inst_player_quickload)
+		slot_ongoing = node_player_list.get_child_count()
+		pass
+	
+
+
+func quicksave_list():
+	var _file = FileAccess.open("user://player_list.dat", FileAccess.WRITE)
+	if _file == null:
+		return
+	var _name_list = []
+	for _child in node_player_list.get_children():
+		if _child is not MarginContainer: 
+			_name_list.erase(_child)
+			continue
+		if (_child.get_node_or_null(
+			"PlayerDecor/PlayerTemplateMargin/PlayerContainer/PlayerNameLabel"
+			) != null ):
+			_name_list.append(_child.get_node_or_null(
+			"PlayerDecor/PlayerTemplateMargin/PlayerContainer/PlayerNameLabel"
+			).text)
+		
+	var _json_data = JSON.stringify(_name_list)
+	_file.store_string(_json_data)
